899906771ed41034eedf3067048333b1c461718b
Kelly Taylor <kellytaylor@Kellys-MacBook-Pro-2.local>
2017-08-21 19:45:42 -0600

    Testing a bunch of things

---
+++ rails-app/app/crap.rb
@@ -0,0 +1,8 @@
+# Instance variable assigned to string
+@instance_var = "Blah"
+
+# Local variable assigned to hash using symbol syntax (Unsupported in crystal)
+local_var = { 
+  cool: "thing",
+  sweet: "test"
+}
